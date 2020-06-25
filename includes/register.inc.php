b736470cec372ee5334f2bd1e352f548e34a3f64
thanhbinh123-tr <66456717+thanhbinh123-tr@users.noreply.github.com>
2020-06-25 12:58:19 +0700

    test3

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -9,7 +9,7 @@
         $email = $_POST['email'];
         $password = $_POST['password'];
         $passwordrp = $_POST['password-rp'];
-        }
+    
         if(empty($name) ||empty($phonenumber) || empty($address)|| empty($email)||empty($password) || empty($passwordrp)){
             header("Location: ../login.php?error=emptyfield");
             exit();
@@ -31,12 +31,13 @@
         }
         else{
             $query = "INSERT INTO Agency (AgencyName, AgencyPhoneNumber, AgencyAddress, Email, Tel ,Password1)
-             VALUES ('".$name."','".$phonenumber."','".$address."','".$email."'," ",'".$password."')";
+             VALUES ('".$name."','".$phonenumber."','".$address."','".$email."','','".$password."')";
             $result = pg_query($conn,$query);
 
             header("Location: ../login.php?register=success");
             exit();
         }
+    }
     else{
         header("location: ../login.php?failed");
         exit();
