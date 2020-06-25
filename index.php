99a44422d067df6288c51ab82722eefeb9032ed8
thanhbinh123-tr <66456717+thanhbinh123-tr@users.noreply.github.com>
2020-06-25 14:03:45 +0700

    test7

--- index.php
+++ index.php
@@ -6,12 +6,14 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JB Studio</title>
     <link rel="stylesheet" href="./Styles/JBStudio.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    
     <script>
         $(document).ready(function() {
             $('#myCarousel').carousel({
